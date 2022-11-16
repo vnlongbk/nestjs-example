82ad731acdb192f5703eb437073ba62fbfbe5110
Long Vo GFT <lgvo@gft.com>
2022-11-16 12:05:32 +0700

    git branch -M main

---
+++ src/main.ts
@@ -0,0 +1,13 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidateInputPipe } from './core/pipes/validate.pipe';
+
+async function bootstrap() {
+    const app = await NestFactory.create(AppModule);
+    // global endpoints prefix
+    app.setGlobalPrefix('api/v1');
+    // handle all user input validation globally
+    app.useGlobalPipes(new ValidateInputPipe());
+    await app.listen(3000);
+}
+bootstrap();
