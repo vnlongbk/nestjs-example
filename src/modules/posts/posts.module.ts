b80e4073689f980ed2fe1f6727bf6e623cbeca08
vnlongbk <vnlongbk@gmail.com>
2022-11-20 00:50:03 +0700

    update docker file

--- src/modules/posts/posts.module.ts
+++ src/modules/posts/posts.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PostsService } from './posts.service';
 import { PostsController } from './posts.controller';
+import { postsProviders } from './post.providers';
 
 @Module({
-  providers: [PostsService],
+  providers: [PostsService, ...postsProviders],
   controllers: [PostsController],
 })
 export class PostsModule {}
